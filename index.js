23dce2241fded4bc6b102e764019d54f1d3fe1a2
”AlenaShkr” <elalshkr@gmail.com>
2020-11-19 00:33:20 +0300

    add method choice song from list

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+function handlerChoiceSong(event) {
+  let url; 
+  if (event.target.className === 'song' || 'singer') {
+
+    url = event.target.parentNode.dataset.url;
+  }
+  if (event.target.className === 'song-item') {
+    url = event.target.dataset.url;
+  }
+  return url;
+}
 window.onload = function load() {
   const canvas = document.querySelector('.canvas');
   const context = canvas.getContext('2d');
@@ -6,6 +17,14 @@ window.onload = function load() {
   context.translate(canvas.clientWidth-10, 180);
   context.rotate(3.14);
   context.drawImage(img, 0, 0, 50, 150);
-  console.log(canvas.clientWidth);
-  canvas.addEventListener('click', () => window.console.log('click!'));
+  //canvas.addEventListener('click', () => window.console.log('click!'));
+  const audio = document.querySelector('.control');
+  const listOfSongs = document.querySelector('.list-songs');
+  listOfSongs.addEventListener('click', ev => {
+    if(ev.target === 'song' || 'singer' || 'song-item') {
+      let audioSource = handlerChoiceSong(ev);
+      audio.src=audioSource;
+      audio.autoplay = true;
+    }
+  });
 };
