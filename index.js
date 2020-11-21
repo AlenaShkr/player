84212039b273d82a33376255a974564d5ee6fbfe
”AlenaShkr” <elalshkr@gmail.com>
2020-11-22 00:50:18 +0300

    fix method image after choice song

--- index.js
+++ index.js
@@ -16,10 +16,9 @@ window.onload = function load() {
   context.save();
   const img = new Image();
   img.src = 'assets/3.png';
-  context.translate(canvas.clientWidth, 200);
-  context.rotate(3.14);
-  context.drawImage(img, -10, 0, 50, 180);
-  
+  context.translate(canvas.width, 0);
+  context.rotate(0);
+  context.drawImage(img, -42, 20, 50, 180);
   //canvas.addEventListener('click', () => window.console.log('click!'));
   const audio = document.querySelector('.control');
   const listOfSongs = document.querySelector('.list-songs');
@@ -31,17 +30,20 @@ window.onload = function load() {
                 console.log(this.duration);
             };
       audio.autoplay = true;
-      context.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
       context.restore();
+      context.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+      
       const imgDisk = new Image();
       imgDisk.src = 'assets/1.png';
-      
       context.drawImage(imgDisk, 2, 10, 270, 270);
       context.save();
-      context.translate(canvas.clientWidth+10, 200);
-      context.rotate(3.3);
-      context.drawImage(img, 25, 0, 50, 180);
+      context.translate(canvas.clientWidth, 0);
+      context.rotate(0.1);
+      context.drawImage(img, -42, 20, 50, 180);
+      // context.rotate(0.5); max angle
+      // context.drawImage(img, -28, 25, 50, 180);
       context.restore();
+
     }
   });
 };
