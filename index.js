983091397243c9126adeadc58b82a5f6554bc554
”AlenaShkr” <elalshkr@gmail.com>
2020-11-21 23:39:09 +0300

    add image after choice song

--- index.js
+++ index.js
@@ -9,14 +9,18 @@ function handlerChoiceSong(event) {
   }
   return url;
 }
+
 window.onload = function load() {
   const canvas = document.querySelector('.canvas');
   const context = canvas.getContext('2d');
+  context.save();
   const img = new Image();
   img.src = 'assets/3.png';
-  context.translate(canvas.clientWidth-10, 180);
+  context.translate(canvas.clientWidth, 200);
   context.rotate(3.14);
-  context.drawImage(img, 0, 0, 50, 150);
+  context.drawImage(img, -10, 0, 50, 180);
+  
+  //context.restore();
   //canvas.addEventListener('click', () => window.console.log('click!'));
   const audio = document.querySelector('.control');
   const listOfSongs = document.querySelector('.list-songs');
@@ -24,7 +28,19 @@ window.onload = function load() {
     if(ev.target === 'song' || 'singer' || 'song-item') {
       let audioSource = handlerChoiceSong(ev);
       audio.src=audioSource;
+      audio.onloadedmetadata = function() {
+                console.log(this.duration);
+            };
       audio.autoplay = true;
+      context.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+      context.restore();
+      const imgDisk = new Image();
+      imgDisk.src = 'assets/1.png';
+      
+      context.drawImage(imgDisk, 2, 10, 270, 270);
+      context.translate(canvas.clientWidth+10, 200);
+      context.rotate(3.3);
+      context.drawImage(img, 25, 0, 50, 180);
     }
   });
 };
